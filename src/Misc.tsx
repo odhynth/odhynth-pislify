a7974dce59210657539b98098c56dc5a19743e58
odhynth <86832194+odhynth@users.noreply.github.com>
2021-07-14 03:18:01 -0700

    Noice

--- src/Misc.tsx
+++ src/Misc.tsx
@@ -13,6 +13,20 @@ export function Link(link:string , name:string, blank:boolean)
     );
 }
 
+export function ButtonLink(link:string , name:string, blank:boolean)
+{
+    return(
+        <div className = "ButtonLink">
+            <a
+            href = {link}
+            target = {blank ? "_blank" : ""}
+        > 
+            {name}
+        </a>
+        </div>
+    );
+}
+
 export class Logo extends Component
 {
     render()
